6f6d48ba9fc91d205825d09f35316e2aa36e5a82
juangaviriaa-alt <juangaviriaa@ielacandelariamedellin.edu.co>
2026-03-17 21:07:24 +0000

    chao

---
+++ 17-03/ciclos.js
@@ -0,0 +1,18 @@
+let num=1
+while(num <=100){
+    console.log(num);
+    num++;
+}
+
+let numero=2
+while(numero <=100){
+    console.log(numero);
+    numero+=2;
+}
+
+let num2=1
+while(num2 <=100){
+    console.log(num2);
+    num2+=2;
+}
+
