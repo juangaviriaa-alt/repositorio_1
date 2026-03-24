64550922a4e909e80cad041c9f5bb7affa2dc560
juangaviriaa-alt <juangaviriaa@ielacandelariamedellin.edu.co>
2026-03-24 20:56:26 +0000

    banano

---
+++ 24-03/ejercicio1_1.js
@@ -0,0 +1,9 @@
+var nombre="Juan";
+let edad=16;
+const ciudad="Melellin";
+
+console.log("mi nombre es "+nombre);
+console.log("mi edad es "+edad);
+console.log("mi ciudad es "+ciudad);
+
+
---
+++ 24-03/ejercicio1_4.js
@@ -0,0 +1,13 @@
+let precio = "350"
+precio=350;
+var numero=100;
+suma=precio+numero;
+
+console.log("precio:", precio, "numero:", numero, "suma:", suma);
+console.log("el tipo de dato es:", typeof(precio));
+
+let="450 pesos colombianos";
+console.log(let);
+
+resultado="es igual"+suma, "en pesos colombianos";
+console.log("el tipo de dato es:", typeof(resultado));
