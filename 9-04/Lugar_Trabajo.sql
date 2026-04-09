00b19b6c3f421097550e38e8b8c3d8671fb09a41
juangaviriaa-alt <juangaviriaa@ielacandelariamedellin.edu.co>
2026-04-09 19:59:26 +0000

    guau

---
+++ 9-04/Lugar_Trabajo.sql
@@ -0,0 +1,8 @@
+Create database Lugar_Trabajo
+Use Lugar_Trabajo
+Create table Lugar_Trabajo(
+Codigo-Int primary key,
+Precio_adomicilio-Int,
+Direccion_Int);
+
+// En el Int se guarda el codigo del lugar de trabajo, igual que el precio adomicilio y lo mismo con la direccion del domicilio
